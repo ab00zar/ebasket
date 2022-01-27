f10fbbd57b2f292a719b1866de25bdceb43f7e09
ab00zar <aboozar.rajabi@plezi.co>
2022-01-27 20:45:33 +0100

    Add a new route for add to basket

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :discounts
-  devise_for :users
-  resources :items
   root to: 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :discounts
+
+  devise_for :users
+
+  resources :items do
+    collection do
+      get ':id/add_to_basket' => 'items#add_to_basket'
+    end
+  end
 end
