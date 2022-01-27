c0445ca11f15de4f99fd762c8aa5b9588bfa39ca
ab00zar <aboozar.rajabi@plezi.co>
2022-01-27 20:40:34 +0100

    Add default values to price attributes

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -4,4 +4,7 @@ class Basket < ApplicationRecord
   has_many :basket_items, dependent: :delete_all
   has_many :items, through: :basket_items
   belongs_to :user
+
+  attribute :price, :integer, default: 0
+  attribute :final_price, :integer, default: 0
 end
--- app/models/basket_item.rb
+++ app/models/basket_item.rb
@@ -3,4 +3,7 @@
 class BasketItem < ApplicationRecord
   belongs_to :item
   belongs_to :basket
+
+  attribute :price, :integer, default: 0
+  attribute :final_price, :integer, default: 0
 end
