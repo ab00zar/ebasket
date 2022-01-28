27c451aaf9c94a86b212ba16b7ef3fa3199a0705
ab00zar <aboozar.rajabi@plezi.co>
2022-01-28 11:59:27 +0100

    Make items editable as well as their discount

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -81,6 +81,6 @@ class ItemsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def item_params
-    params.require(:item).permit(:name, :code, :price)
+    params.require(:item).permit(:name, :code, :price, :discount_id)
   end
 end
