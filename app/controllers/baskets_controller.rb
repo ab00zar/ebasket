ad76c137c701a851d408a3823a92ee1d4185e9ef
ab00zar <aboozar.rajabi@plezi.co>
2022-01-28 11:39:06 +0100

    Reset option is added

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class BasketsController < ApplicationController
-  before_action :set_basket, only: %i[show]
+  before_action :set_basket, only: %i[show reset]
 
   def show
     @basket_items = @basket.basket_items.order(updated_at: :asc)
+  end
+
+  def reset
+    @basket.update(price: 0, final_price: 0)
+    BasketItem.delete_all
+    respond_to do |format|
+      format.html { redirect_to my_basket_path, notice: 'Reset is done!' }
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   end
 
   get 'my_basket' => 'baskets#show'
+  get 'reset' => 'baskets#reset'
 end
